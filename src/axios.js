21ea1f2447f3ab8408a0177244cedd072f5bda52
Tahmid Khandokar <tahmidkhandokar82@gmail.com>
2022-06-30 09:17:42 +0600

    axios baseUrl changed

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:9000",
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? "https://blockchain-bank-app-demo.herokuapp.com/"
+      : "http://localhost:9000",
 });
 
 export default instance;
